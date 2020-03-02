4fa384aef61510ba7b71ee2a162cdd16a85dbbee
Tornado3P9 <max.barzantny@yahoo.de>
2020-03-02 18:28:47 +0100

    added jq

--- webinstall.sh
+++ webinstall.sh
@@ -14,8 +14,8 @@ sudo add-apt-repository ppa:stebbins/handbrake-releases
 ##############################
 sudo apt update
 sudo apt upgrade
-sudo apt install git snapd gparted synaptic xubuntu-restricted-extras vlc python3-pip mpv gimp xsane pdfshuffler pinta audacity bless htop mypaint sysinfo hardinfo acpi
-sudo apt install sqlite3 nodejs npm gaupol mediainfo mediainfo-gui nethogs wireshark guvcview lxsplit libimage-exiftool-perl perl-doc steam kazam vpnc xsensors handbrake-gtk
+sudo apt install git snapd gparted synaptic xubuntu-restricted-extras vlc python3-pip mpv jq gimp xsane pdfshuffler pinta audacity bless htop mypaint sysinfo hardinfo acpi
+sudo apt install sqlite3 nodejs npm gaupol mediainfo mediainfo-gui nethogs wireshark guvcview lxsplit libimage-exiftool-perl perl-doc steam kazam vpnc xsensors handbrake-gtk hugin
 ##############################
 # Install line snap          #
 ##############################
