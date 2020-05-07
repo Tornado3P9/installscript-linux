c2dcdcc4562bc91737569f987cd2cc171f078e24
Tornado3P9 <max.barzantny@yahoo.de>
2020-05-07 11:32:24 +0200

    added appimages

--- webinstall.sh
+++ webinstall.sh
@@ -15,22 +15,24 @@ add-apt-repository ppa:cteehayder/ffmulticonverter
 ##############################
 apt update
 apt upgrade
-apt install git snapd gparted synaptic xubuntu-restricted-extras vlc python3-pip mpv jq gimp xsane pdfshuffler pinta audacity bless htop mypaint sysinfo hardinfo acpi libreoffice libreoffice-l10n-de libreoffice-help-de
+apt install git snapd gparted synaptic xubuntu-restricted-extras python3-pip mpv jq gimp xsane pdfshuffler pinta audacity bless htop sysinfo hardinfo acpi libreoffice libreoffice-l10n-de libreoffice-help-de
 apt install sqlite3 nodejs npm gaupol mediainfo mediainfo-gui nethogs wireshark guvcview lxsplit libimage-exiftool-perl perl-doc steam kazam vpnc xsensors handbrake-gtk hugin gifsicle wget tree ufw gufw ffmulticonverter arp-scan nmap zenmap
+#apt install mypaint vlc
 ##############################
 # Install line snap          #
 ##############################
 # https://snapcraft.io/store hier suchen, ob snap das gewuenschte Programm ueberhaupt hat.
-snap install android-studio --classic
-snap install eclipse --classic
-snap install fast inkscape krita meshlab discord zoom-client kdenlive pick-colour-picker
+snap install fast discord pick-colour-picker
 snap install heroku --classic
 snap install micro --classic
-snap install pycharm-community --classic
 # pick-colour-picker https://kryogenix.org/code/pick/
-#sudo snap install code --classic
-#sudo snap install blender --classic
-#sudo snap install skype --classic
+#snap install pycharm-community --classic
+#snap install android-studio --classic
+#snap install eclipse --classic
+#snap install code --classic
+#snap install blender --classic
+#snap install skype --classic
+#snap install inkscape krita meshlab zoom-client kdenlive
 ##############################
 # Install line pip3          #
 ##############################
@@ -39,7 +41,7 @@ pip3 install --user virtualenv selenium pytest requests flask youtube-dl
 ##############################
 # Install additional items   #
 ##############################
-# Do you want to install Texlive and Texmaker?
+# 1. Do you want to install Texlive and Texmaker?
 read -p "Do you want to install Texlive and Texmaker? [Y/n]: " yn
 if [ "$yn" != "n" ]; then
   apt install texlive-full texmaker
@@ -48,56 +50,65 @@ read -p "Do you want to copy old alias? [Y/n]: " yn2
 if [ "$yn2" != "n" ]; then
   cat alias.txt >> ~/.bashrc
 fi
-### Check Firewall Status https://wiki.ubuntuusers.de/ufw/:
+### 2. Check Firewall Status https://wiki.ubuntuusers.de/ufw/:
 ufw status
 read -p "Do you want to activate firewall? [Y/n]: " yn3
 if [ "$yn3" != "n" ]; then
   ufw enable
 # ufw disable
 fi
-### Automaticly enter 'yes' when asked:
-#for i in program1 program2 program3; do
-#  apt-get install $i -y
-#done
 #
-### Additional Themes:
+### 3. Additional Themes:
 apt install arc-theme moka-icon-theme numix-icon-theme numix-gtk-theme lightdm-gtk-greeter-settings
 #
-### INSTALLATION VON EXTERNEN PROGRAMMEN, BEISPIEL:
-#Externe Programme herunterladen:
-#z.B. MicrosoftTeams, Blender, Sublime-Text, VSCode, JetBrains WebStorm/Rider/PyCharm IDE, UnityHub,
-#     magick https://imagemagick.org/script/download.php,
-#     ffmulticonverter https://sites.google.com/site/ffmulticonverter/download,
-#     mono https://www.mono-project.com/download/stable/
-#
-#Ordner erstellen:
+### 4. Ordner Fuer Externe Programme Erstellen:
 mkdir ~/Programs
-#Debian packages nach Programs kopieren und(&&) in diesen Ordner wechseln:
-# cp ~/Downloads/*.deb ~/Programs && cd ~/Programs
-#Debians installieren: sudo apt install ./*.deb
-#                oder: sudo dpkg -i *.deb
 #
-### Install/Extract blender and copy to Programs folder:
+### 5. Installation Von Externen Programmen:
+#A: Programme Herunterladen, z.B.:
+# MicrosoftTeams, Blender, Sublime-Text, VSCode, JetBrains WebStorm/Rider/PyCharm IDE,
+# magick https://imagemagick.org/script/download.php,
+# ffmulticonverter https://sites.google.com/site/ffmulticonverter/download,
+# mono https://www.mono-project.com/download/stable/
+# zoom-client https://zoom.us/download
+# Appimages z.B.:
+# krita https://krita.org/,
+# inkscape https://inkscape.org/,
+# MyPaint http://mypaint.org/,
+# Stellarium http://stellarium.org/,
+# Unity-Hub https://unity3d.com/get-unity/download,
+# Kdenlive https://kdenlive.org/en/download/
+#
+#B: Debian packages nach Programs kopieren und(&&) in diesen Ordner wechseln:
+# cp ~/Downloads/*.deb ~/Programs && cd ~/Programs
+# installieren per: sudo apt install ./*.deb
+#             oder: sudo dpkg -i *.deb
+#
+#C: Move AppImages to ~/Programs and make executable
+# mv ~/Downloads/*.AppImage ~/Programs
+# chmod +x ~/Programs/*.AppImage
+#
+#D: Extract blender and copy to Programs folder:
 # tar -xvf ~/Downloads/blender*.tar.xz -C ~/Programs
 #Und installiere VSE_Transform_Tools fuer Videobearbeitung: https://github.com/doakey3/VSE_Transform_Tools
 #Und GIF-Import: https://github.com/doakey3/Bligify
 #Und CAD_Tool: https://github.com/Laurent26/techdraw
 #Und JMesh_Hard_Surface_Tools: https://github.com/jayanam/jmesh-tools
 #
-### Konsolentools herunterladen:
+### 6. Konsolentools herunterladen:
 # cd ~/Programs && git clone https://github.com/Tornado3P9/Konsolentools
 #
-### Shortcut Collection herunterladen, nach ~/Programs entpacken und zip-Archiv loeschen:
+### 7. Shortcut Collection herunterladen, nach ~/Programs entpacken und zip-Archiv loeschen:
 wget https://www.dropbox.com/sh/svkokavmhty8xi3/AADawdnN94IPQqoE7obCAOLNa?dl=0 -O gezipptesArchiv.zip
 unzip gezipptesArchiv.zip -d ~/Programs/
 rm gezipptesArchiv.zip
 #
-### xfce4-settings-manager (Alt+F2 -> xfce4-)
+### 8. xfce4-settings-manager (Alt+F2 -> xfce4-)
 echo "xfce4-settings-manager -> Fenstereinstellungen der Fensterverwaltung -> Zugreifbarkeit -> Taste zum nehmen und verschieben der Fenster: None"
 read -p "EnterTaste:"
 xfce4-settings-manager
 #
-### TASTENKUERZEL FUER LIEBLINGSPROGRAMME ERSTELLEN: xfce4-settings-manager -> Tastatur -> Tastenkuerzel
+### 9. TASTENKUERZEL FUER LIEBLINGSPROGRAMME ERSTELLEN: xfce4-settings-manager -> Tastatur -> Tastenkuerzel
 #Strg+Alt+Entf -> xfce4-session-logout
 #Super+W       -> Webbrowser
 #Super+F       -> Dateibrowser(Home Folder)
@@ -106,7 +117,11 @@ xfce4-settings-manager
 #Super+L       -> Bildschirm sperren
 #Umschalt+Strg+Esc -> xfce4-taskmanager
 #
-### Bildschirm Hintergrund:
+### 10. Virtuelle Python Environment erstellen, siehe in alias.txt unter 'virtual environments workflow'
+mkdir ~/my-python-venv && cd ~/my-python-venv
+python3 -m venv ./venv
+#
+### 11. Bildschirm Hintergrund:
 # https://www.pexels.com/
 # https://www.pexels.com/photo/black-and-white-mountain-over-yellow-white-and-blue-sky-46253/
 #
@@ -122,4 +137,9 @@ xfce4-settings-manager
 # Inkscape (Vektorgrafiken) https://inkscape.org/
 # MyPaint (Professionelles Maltool fuer Arbeit am Digital Drawing Tablet) http://mypaint.org/
 # Blender2.8 (Professionelles 2D-Maltool fuer Arbeit am Digital Drawing Tablet) https://www.blender.org/
+#
+### Automaticly enter 'yes' when asked:
+#for i in program1 program2 program3; do
+#  apt-get install $i -y
+#done
 exit 0
